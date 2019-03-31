3857f4b744c504e2011b9a2900cd7be4e59b8918
Nedu Robert <nedumrobert@gmail.com>
2019-03-31 09:14:07 +0100

    Feat: Products component

---
+++ src/components/Products.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import '../assets/styles/main.scss';
+
+const Products = ({ products }) =>
+    <div className="productsContainer">
+        <h1>Products Grid</h1>
+        { /* eslint-disable-next-line react/no-unescaped-entities */}
+        <p>Here you're sure to find a bargain on some of the finest ascii available to purchase. Be sure to peruse our selection of ascii faces in an exciting range of sizes and prices.</p>
+        <div className="list">
+            {products.map(item => <div className="list-row" key={item.id}>
+                <div className="productFace" style={{fontSize: item.size}}>{item.face}</div>
+                <div className="productPrice">{`price: $${(item.price / 100).toFixed(2)}`}</div>
+                <div className="productSize">{`font size: ${item.size} px`}</div>
+                <div className="productDate">{item.date}</div>
+            </div> )}
+        </div>
+    </div>
+
+export default Products
